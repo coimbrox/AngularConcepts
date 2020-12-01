b2a4d26e7dd5b48e559677a528b0dc105f07b1d8
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-12-01 19:41:45 -0300

    Criando Componente Topo

---
+++ app1/src/app/top/top.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+//Decorator - Permite encaminhar Metadados por JSON ou js
+@Component({
+  selector: 'app-top',
+  templateUrl: './top.component.html'
+})
+
+export class TopComponent { }
