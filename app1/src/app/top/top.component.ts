16a626580f68aa2ebb4a5ad7438a42c774eb8c2d
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-12-01 19:59:32 -0300

    Top Component Style

--- app1/src/app/top/top.component.ts
+++ app1/src/app/top/top.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core'
 //Decorator - Permite encaminhar Metadados por JSON ou js
 @Component({
   selector: 'app-top',
-  templateUrl: './top.component.html'
+  templateUrl: './top.component.html',
+  styleUrls: ['./top.component.css']
+
 })
 
 export class TopComponent { }
