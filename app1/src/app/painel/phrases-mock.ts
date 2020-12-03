db646f1e1d9d958db92e808584afc3b325d883de
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-12-03 20:16:54 -0300

    Create Phrase-Mock

---
+++ app1/src/app/painel/phrases-mock.ts
@@ -0,0 +1,8 @@
+import { Phrase } from '../shared/phrase.model'
+
+const PHRASES: Phrase[] = [
+  { PhrasheEng: 'I like to learn', PhrasePtbr: 'Eu Gosto de Aprender' },
+  { PhrasheEng: 'I watch Tv', PhrasePtbr: 'Eu assisto TV' },
+  { PhrasheEng: 'How are you?', PhrasePtbr: 'Como vai você?' },
+  { PhrasheEng: 'I eat bread', PhrasePtbr: 'Eu como pão' }
+]
