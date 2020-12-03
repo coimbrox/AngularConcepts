211a587fa46ca157d374ec2ff582ae740443f94b
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-12-03 20:10:14 -0300

    Create Class Phrase

---
+++ app1/src/app/shared/phrase.model.ts
@@ -0,0 +1,8 @@
+export class Phrase {
+  // public PhraseEng: string
+  // public PhrasePtbr: string
+  constructor(public PhrasheEng: string, public PhrasePtbr: string) {
+    // this.PhraseEng = PhrasheEng
+    // this.PhrasePtbr = PhrasePtbr
+  }
+}
