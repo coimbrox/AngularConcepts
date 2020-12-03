9cdf53ea1930fbd3ec3b999cd8b9a39a44eb371e
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-12-03 17:17:47 -0300

    Property Binding

--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentativasComponent implements OnInit {
 
+  public EmptyHeart: string = '../../assets/coracao_vazio.png'
+  public FullHeart: string = '../../assets/coracao_cheio.png'
+
   constructor() { }
 
   ngOnInit(): void {
